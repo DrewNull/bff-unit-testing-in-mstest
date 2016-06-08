7763cef230584258f2626e4c9c8951413afc92be
Joe Meyer <JMeyer@rightpoint.com>
2016-06-08 16:03:14 -0500

    Stub project + test

---
+++ BFF.UnitTestingInMSTest/BFF.UnitTestingInMSTest.Test/PhoneTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BFF.UnitTestingInMsTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BFF.UnitTestingInMSTest.Test
+{
+    [TestClass]
+    public class PhoneTest
+    {
+        private Phone TestPhone;
+
+        public PhoneTest()
+        {
+            TestPhone = new Phone();            
+        }
+
+        [TestMethod]
+        public void AlphanumericToPhoneNumber_HasValidPhoneWord_ReturnsAllNumbers()
+        {
+            // arrange
+            var expectedNumber = "1234";    //TODO
+            var alphanumericPhone = "1-800-CAL-RTPT";   //TODO
+
+            // act
+            var result = TestPhone.AlphanumericToPhoneNumber(alphanumericPhone);
+
+            // assert
+            //TODO
+        }
+    }
+}
---
+++ BFF.UnitTestingInMSTest/BFF.UnitTestingInMsTest/Phone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFF.UnitTestingInMsTest
+{
+    public class Phone
+    {
+        public string AlphanumericToPhoneNumber(string phoneword)
+        {
+            string result = string.Empty;
+            foreach (var character in phoneword)
+            {
+                if ("ABC".Contains(character))
+                {
+                    result = string.Concat(result, "1");
+                }
+                else if ("DEF".Contains(character))
+                {
+                    result = string.Concat(result, "2");
+                }
+            }
+            return result;
+        }
+    }
+}
